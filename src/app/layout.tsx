3eda9056b825698442e2b7c39955c018e527ceda
Nori <s.norihama@gmail.com>
2026-03-19 15:34:30 +0900

    changed directory to norihamaro.com

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,38 +15,38 @@ const zenOldMincho = Zen_Old_Mincho({
 });
 
 export const metadata: Metadata = {
-	metadataBase: new URL("https://norihamaro.vercel.app/"),
+	metadataBase: new URL("https://www.norihamaro.com/"),
 	title: "東大麻婆愛好会",
 	description: "本当に美味しい麻婆豆腐をあなたはまだ知らない。",
 	icons: {
 		icon: [
 			{
-				url: "https://norihamaro.vercel.app/icons/club-icon_16x16.ico",
+				url: "https://www.norihamaro.com/icons/club-icon_16x16.ico",
 				sizes: "16x16",
 				type: "image/x-icon",
 			},
 			{
-				url: "https://norihamaro.vercel.app/icons/club-icon_32x32.ico",
+				url: "https://www.norihamaro.com/icons/club-icon_32x32.ico",
 				sizes: "32x32",
 				type: "image/x-icon",
 			},
 			{
-				url: "https://norihamaro.vercel.app/icons/club-icon_48x48.ico",
+				url: "https://www.norihamaro.com/icons/club-icon_48x48.ico",
 				sizes: "48x48",
 				type: "image/x-icon",
 			},
 			{
-				url: "https://norihamaro.vercel.app/icons/club-icon_64x64.ico",
+				url: "https://www.norihamaro.com/icons/club-icon_64x64.ico",
 				sizes: "64x64",
 				type: "image/x-icon",
 			},
 			{
-				url: "https://norihamaro.vercel.app/icons/club-icon_128x128.ico",
+				url: "https://www.norihamaro.com/icons/club-icon_128x128.ico",
 				sizes: "128x128",
 				type: "image/x-icon",
 			},
 			{
-				url: "https://norihamaro.vercel.app/icons/club-icon_256x256.ico",
+				url: "https://www.norihamaro.com/icons/club-icon_256x256.ico",
 				sizes: "256x256",
 				type: "image/x-icon",
 			},
@@ -55,10 +55,10 @@ export const metadata: Metadata = {
 	openGraph: {
 		title: "東大麻婆愛好会",
 		description: "本当に美味しい麻婆豆腐をあなたはまだ知らない。",
-		url: "https://norihamaro.vercel.app/",
+		url: "https://www.norihamaro.com/",
 		images: [
 			{
-				url: "https://norihamaro.vercel.app/og/og-newcomer_compressed.png",
+				url: "https://www.norihamaro.com/og/og-newcomer_compressed.png",
 				width: 1200,
 				height: 630,
 				alt: "東大麻婆愛好会",
@@ -70,7 +70,7 @@ export const metadata: Metadata = {
 		card: "summary_large_image",
 		title: "東大麻婆愛好会",
 		description: "本当に美味しい麻婆豆腐をあなたはまだ知らない。",
-		images: ["https://norihamaro.vercel.app/og/og-newcomer_compressed.png"],
+		images: ["https://www.norihamaro.com/og/og-newcomer_compressed.png"],
 	},
 };
 
